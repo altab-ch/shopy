91126f7080da9826aeaf5aa7d7fb36384a133ced
Mathieu Knecht <mathieu.knecht@gmail.com>
2020-10-03 06:58:38 +0200

    setup firebase

--- packages/backend_repository/lib/src/backend_repository.dart
+++ packages/backend_repository/lib/src/backend_repository.dart
@@ -5,27 +5,25 @@ import 'package:backend_repository/backend_repository.dart';
 import 'entities/entities.dart';
 
 class FirebaseTodosRepository {
-  final todoCollection = Firestore.instance.collection('todos');
+  final todoCollection = FirebaseFirestore.instance.collection('todos');
 
   Future<void> addNewTodo(Receipt todo) {
     return todoCollection.add(todo.toEntity().toDocument());
   }
 
   Future<void> deleteTodo(Receipt todo) async {
-    return todoCollection.document(todo.id).delete();
+    return todoCollection.doc(todo.id).delete();
   }
 
   Stream<List<Receipt>> todos() {
     return todoCollection.snapshots().map((snapshot) {
-      return snapshot.documents
+      return snapshot.docs
           .map((doc) => Receipt.fromEntity(ReceiptEntity.fromSnapshot(doc)))
           .toList();
     });
   }
 
   Future<void> updateTodo(Receipt update) {
-    return todoCollection
-        .document(update.id)
-        .updateData(update.toEntity().toDocument());
+    return todoCollection.doc(update.id).update(update.toEntity().toDocument());
   }
 }
